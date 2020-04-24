95cce5e6e8f4569dde6419ab0579516b9dc553c7
Dwight Nodolf <dnodolf@gmail.com>
2020-04-24 05:46:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,14 +10,11 @@ def actor
 end
 
 def monopoly
-	monopoly = {
-	  :railroads => {:pieces => 4, :names => {}}}
-
+	monopoly = {}
 end
 
 def monopoly_with_second_tier
-	monopoly = {:railroads => {:pieces => 4}
-
+	
 end
 
 def monopoly_with_third_tier
